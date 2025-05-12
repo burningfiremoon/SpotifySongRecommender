b388e1676761e4d2ebba07b501b32015df85fb3c
burningfiremoon <charleszhang4@gmail.com>
2025-05-12 08:24:05 -0700

    load and dump util functions done

---
+++ utilities.py
@@ -0,0 +1,18 @@
+import time
+from sklearn.cluster import KMeans
+import joblib
+
+def generate_file_name(model: KMeans, directory: str = "./models") -> str:
+    timeStamp = time.strftime("%Y%m%d-%H%M")
+    modelName = type(model).__name__
+    return f"{directory}/{modelName}_{timeStamp}"
+
+
+def dump_model(model: KMeans) -> str:
+    fileName = generate_file_name(model)
+    joblib.dump(model, fileName)
+    return fileName
+
+
+def load_model(directory: str) -> KMeans:
+    return joblib.load(directory)
