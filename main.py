5a258bfefa54f1d1ea70d8701cc4ad9903499fe8
burningfiremoon <charleszhang4@gmail.com>
2025-05-21 22:11:00 -0700

    can extract popularity

--- main.py
+++ main.py
@@ -6,6 +6,30 @@ from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth 
 from spotipy.cache_handler import FlaskSessionCacheHandler
 
+#pandas imports
+import pandas as pd
+
+song_data = pd.read_csv('models/tracks_features.csv')
+
+songIDs = song_data[['id', 'name']]
+
+testSongs = [
+    '7lmeHLHBe4nmXzuXc0HDjk',
+    '1wsRitfRRtWyEapl0q22o8',
+    '1hR0fIFK2qRG3f3RF70pb7',
+    '2lbASgTSoDO7MTuLAXlTW0',
+    '1MQTmpYOZ6fcMQc56Hdo7T'
+    ]
+
+testSongNames = [
+    'Testify',
+    'Guerrilla Radio',
+    'Calm Like a Bomb',
+    'Mic Check',
+    'Sleep Now In the Fire'
+]
+
+
 client_id = os.getenv('CLIENT_ID')
 client_secret = os.getenv('CLIENT_SECRET')
 redirect_uri = os.getenv('REDIRECT_URI')
@@ -38,7 +62,7 @@ sp = Spotify(auth_manager=sp_oauth)
 def home():
     # if user is not logged in
     get_token()
-    return redirect(url_for('get_playlists'))
+    return redirect(url_for('get_features'))
 
 # creating end-point to refresh expired token
 # user doesn't have to re log-in
@@ -46,7 +70,7 @@ def home():
 @app.route('/callback')
 def callback():
     sp_oauth.get_access_token(request.args['code'])
-    return redirect(url_for('get_playlists'))
+    return redirect(url_for('get_features'))
 
 # get playlist example
 @app.route('/get_playlists')
@@ -58,6 +82,27 @@ def get_playlists():
     playlists_html = '<br>'.join([f'{name}: {url}' for name, url in playlists_info])
 
     return playlists_html
+
+@app.route('/get_features')
+def get_features():
+    get_token()
+
+    songs = sp.tracks(tracks=testSongs)
+    songs_data = []
+
+    for track in songs['tracks']:
+        song_info = {
+            'name': track['name'],
+            'id': track['id'],
+            'popularity': track['popularity']
+        }
+        songs_data.append(song_info)
+    
+    print(len(songs_data))
+
+    songs_html = '<br>'.join([f'popularity: {track["popularity"]} {track["name"]}: {track["id"]}' for track in songs_data])
+
+    return songs_html
 
 @app.route('/logout')
 def logout():
