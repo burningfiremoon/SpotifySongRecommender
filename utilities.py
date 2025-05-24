0c4fabbc266c7919e2255a9e84050ba82bfc2b55
Chrome503337 <jadlarenlu@gmail.com>
2025-05-23 23:23:28 -0700

    Added util and miniBatchkmeans

--- utilities.py
+++ utilities.py
@@ -1,6 +1,7 @@
 import time
 from sklearn.cluster import KMeans
 import joblib
+import boto3
 
 def generate_file_name(model: KMeans, directory: str = "./models") -> str:
     timeStamp = time.strftime("%Y%m%d-%H%M")
@@ -16,3 +17,7 @@ def dump_model(model: KMeans) -> str:
 
 def load_model(directory: str) -> KMeans:
     return joblib.load(directory)
+
+def save_data_frame(song_data, filename="song_data.csv"):
+    song_data.to_csv(filename, index=False)
+    print(f"Saved file as {filename}")
