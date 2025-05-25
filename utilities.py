7132cf08df1ec6b4d7f6af0932cfccbcda8b1a21
Chrome503337 <jadlarenlu@gmail.com>
2025-05-24 21:19:32 -0700

    added to requirements.txt

--- utilities.py
+++ utilities.py
@@ -18,6 +18,3 @@ def dump_model(model: KMeans) -> str:
 def load_model(directory: str) -> KMeans:
     return joblib.load(directory)
 
-def save_data_frame(song_data, filename="song_data.csv"):
-    song_data.to_csv(filename, index=False)
-    print(f"Saved file as {filename}")
