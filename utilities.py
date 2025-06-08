31c49a78e230482a887dd71107f3d583a2276def
burningfiremoon <charleszhang4@gmail.com>
2025-06-08 16:18:16 -0700

    set and delete finished - not tested

--- utilities.py
+++ utilities.py
@@ -4,6 +4,7 @@ import joblib
 import boto3
 import mysql.connector
 from mysql.connector.cursor import MySQLCursor
+from enum import Enum
 
 class Track:
     track_id:str
@@ -13,7 +14,6 @@ class Track:
         self.track_id = track_id
         self.song_name = song_name
         self.artist_name = artist_name
-
 
 def generate_file_name(model: KMeans, directory: str = "./models") -> str:
     timeStamp = time.strftime("%Y%m%d-%H%M")
@@ -31,7 +31,6 @@ def load_model(directory: str) -> KMeans:
     return joblib.load(directory)
 
 # SQL FUNCTION
-
 """=======================
 searches for Track using track_id
 
@@ -44,9 +43,26 @@ def contain_track(cursor: MySQLCursor, track_id: str) -> bool:
 """=======================
 sets Track to sql database
 
+returns:
+    True: if Track successfully set
+    False: if Track already exist
 ======================="""
-def set_track(cursor: MySQLCursor, song: Track):
-    pass
+def set_track(cursor: MySQLCursor, song: Track) -> bool:
+    # duplicate ID check
+    if contain_track(cursor=cursor, track_id=song.track_id):
+        return False
+    
+    # USE is similar to change director (cd)
+    insert_query = """
+        INSERT INTO tracks (track_id, song_name, artist_name)
+        VALUES (%s, %s, %s)
+    """
+    values = (song.track_id, song.song_name, song.artist_name)
+
+    cursor.execute(insert_query, values)
+    # cursor.commit() # leave this to the caller? (better practice)
+    
+    return True
 
 """=======================
 delete track from SQL database
@@ -57,7 +73,16 @@ returns:
 
 ======================="""
 def delete_track(cursor: MySQLCursor, track_id: str) -> bool:
-    pass
+    # duplicate ID check
+    if !contain_track(cursor=cursor, track_id=track_id):
+        return False
+    
+    delete_query = """
+        DELETE FROM tracks
+        WHERE track_id = %s
+    """
+    cursor.execute(delete_query, (track_id,))
+    return cursor.rowcount > 0 # returns True if something was deleted
 
 """=======================
 gets a Track from database
