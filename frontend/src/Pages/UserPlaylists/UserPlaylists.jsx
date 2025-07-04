70790aa543b84d0c93163665acca08a8aa220034
burningfiremoon <charleszhang4@gmail.com>
2025-07-04 15:07:22 -0700

    generate and send json done, but not tested

--- frontend/src/Pages/UserPlaylists/UserPlaylists.jsx
+++ frontend/src/Pages/UserPlaylists/UserPlaylists.jsx
@@ -8,7 +8,6 @@ function UserPlaylists() {
     const [playlists, setPlaylists] = useState([]);
     const [selectedPlaylists, setSelectedPlaylists] = useState([]);
     const [token, setToken] = useState("");
-    const [tracks, setTracks] = useState([]);
 
     // Getting token
     useEffect(() => {
@@ -48,7 +47,25 @@ function UserPlaylists() {
         setPlaylists([...playlists, playlist]);
     }
 
+    const sendJsonToBackend = async (data) => {
+        try {
+            const response = await fetch('http://127.0.0.1:5000/generate',{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+
+            const result = await response.json();
+            console.log('Server response: ', result);
+        } catch (err){
+            console.log('Error sending JSON', err);
+        }
+    }
+
     const generateJson = async (playlists, _token = token) => {
+        const batchSize = 100;
 
         // All song Ids and popularity is generated and stored
         try {
@@ -67,12 +84,29 @@ function UserPlaylists() {
                     };
                 })
             );
-            console.log("All tracks:", allTracks)
+            // console.log("All tracks:", allTracks)
+            // Fetch to get song_id, tempo, loudness, energy, danceability, liveness, speechiness, acousticness, instrumentalness, valence
+            let allAudioFeatures = [];
+            for (let i=0; i < allTracks.length; i += batchSize){
+                const batch = allTracks.slice(i, i + batchSize);
+                const idsParam = batch.map(allTracks => allTracks.id).join(',');
+
+                const res = await fetch(`https://api.spotify.com/v1/audio-features?ids=${idsParam}`,{
+                    headers: {
+                        Authorization: `Bearer ${_token}`,
+                    },
+                });
+
+                const data = await res.json();
+                allAudioFeatures.push(...data.audio_features);
+            }
+
+            sendJsonToBackend(allAudioFeatures);
+
         } catch (err){
             console.error("Error generating playlist JSON:", err);
         };
-        
-        // Fetch to get song_id, tempo, loudness, energy, danceability, liveness, speechiness, acousticness, instrumentalness, valence
+
     }
 
   return (
